c635cd31402768957de3528dd7ca3f673da3f3db
Afterglow <cedric@moriskelly.com>
2026-06-01 21:17:34 +0200

    removed unecessary lint dev deps

--- remote-media-sync.php
+++ remote-media-sync.php
@@ -6,7 +6,7 @@
  * Version: 1.0.0
  * Requires at least: 6.0
  * Requires PHP: 7.4
- * Author: Cédric Moris Kelly
+ * Author: bromate
  * Author URI: https://www.moriskelly.com
  * License: GPL-2.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
